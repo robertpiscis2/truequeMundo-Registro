6a8f0d7784ec6801bf1ba1ad1fd148cd70e35da1
Roberto <42901082+robertpiscis2@users.noreply.github.com>
2021-02-25 10:21:40 -0500

    last commit

---
+++ js/my-scripts/script-signup.js
@@ -0,0 +1,49 @@
+const btnSignup = document.getElementById("btnSubmitSignup");
+
+btnSignup.addEventListener("click", submitDataSignup);
+
+async function submitDataSignup() {
+  const firstName = document.getElementById("frmFirstName");
+  const lastName = document.getElementById("frmLastName");
+  const email = document.getElementById("frmEmail");
+  const password = document.getElementById("frmPassword");
+
+  const validation = validateFields(
+    firstName.value,
+    lastName.value,
+    email.value,
+    password.value
+  );
+
+  if (validation == "ok") {
+    const url = "http://truequemundo.xyz/events/new_usuario";
+
+    const dataDetails = {
+      method: "POST",
+      body: JSON.stringify({
+        nombre: firstName.value,
+        apellido: lastName.value,
+        email: email.value,
+        password: password.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(url, dataDetails);
+
+    const data = await response.text();
+
+    alert("El usuario ha sido creado");
+    setTimeout(10000);
+    window.location.href = "https://www.truequemundo.online/";
+  } else {
+    alert("Los espacios no pueden estar en blanco");
+  }
+}
+
+function validateFields(fName, lName, email, pass) {
+  if (fName == "" || lName == "" || email == "" || pass == "") return "error";
+  else return "ok";
+}
